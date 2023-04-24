9525a124f04545f9b75367cc49921a06adf72d4e
Vladimir2077 <karlbernadot77@yandex.ru>
2023-04-24 12:16:56 +0200

    Added homework

---
+++ №3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите два положительных числа: M и N.");
+int m = InputInt("Введите m: ");
+int n = InputInt("Введите n: ");
+if (n < 1 || m < 1 )
+{
+    Console.WriteLine("Вы ввели отрицательное число!");
+}
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
